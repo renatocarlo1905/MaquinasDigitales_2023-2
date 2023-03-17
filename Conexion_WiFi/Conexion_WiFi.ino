54990ed5ee7d39c1d46f286581e5ae0018258cab
Carlo Renato <renato.carlo19@comunidad.unam.mx>
2023-03-17 17:45:22 -0600

    Se agregó el programa Conexion_WiFi

---
+++ Conexion_WiFi/Conexion_WiFi.ino
@@ -0,0 +1,32 @@
+#include <WiFi.h>
+#include <credentials.h>
+
+void setup() {
+  Serial.begin(115200);
+    delay(10);
+
+    // We start by connecting to a WiFi network
+
+    Serial.println();
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(SSID);
+
+    WiFi.begin(SSID, PASSWORD);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
