d29ffc7b2d90f010f4098d5e0bc1ee4bec6be7c9
Carlo Renato <renato.carlo19@comunidad.unam.mx>
2023-03-23 14:36:54 -0600

    Se añadió el programa IntButton

---
+++ IntButton/IntButton.ino
@@ -0,0 +1,25 @@
+#define PIN_BUTTON 34
+int numBotPres = 0;
+uint32_t lastMillis = 0;
+
+void IRAM_ATTR isrButton(){
+
+  if (millis() - lastMillis > 5){
+    numBotPres += 1;
+    lastMillis = millis();
+  }
+
+}
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(PIN_BUTTON, INPUT);
+
+  attachInterrupt(PIN_BUTTON, isrButton, RISING);
+
+}
+
+void loop() {
+  Serial.printf("El botón se ha presionado %u veces\n", numBotPres);
+
+}
